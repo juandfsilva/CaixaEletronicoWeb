4e36c0087581173963dda3fcbbb4a42add0e1f53
Marco Quirino <majr_marco@live.com>
2015-09-12 14:56:10 -0300

    Divisão de trabalhos

--- Caixa Eletronico/src/br/usjt/caixaeletronico/Main.java
+++ Caixa Eletronico/src/br/usjt/caixaeletronico/Main.java
@@ -1,10 +1,12 @@
 package br.usjt.caixaeletronico;
 
+import br.usjt.caixaeletronico.view.PeriodoExtrato;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		PeriodoExtrato periodo = new PeriodoExtrato();
 	}
 
 }
